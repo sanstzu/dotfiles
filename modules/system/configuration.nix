9291c0d039e056456f393dbf9d0468a009676414
Clayton Fernalo <claytonfernalo@gmail.com>
2025-02-17 22:08:04 +0800

    feat: enable usb services

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -160,6 +160,9 @@
     sbctl
   ];
 
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
