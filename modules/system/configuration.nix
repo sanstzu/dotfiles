1df919b11360e8366b8bbdd52d0c9171473af3da
Clayton Fernalo <claytonfernalo@gmail.com>
2025-02-27 03:05:08 +0800

    feat: update hyprlock

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -71,15 +71,31 @@
   # Enable the GNOME Desktop Environment.
   services.xserver = {
 		enable = true;
-		displayManager = {
-			gdm.enable = true;
-			gdm.wayland = true;
-		};
 		xkb = {
 			layout = "us";	
 			variant = "";
 		};
-    desktopManager.gnome.enable=true;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${pkgs.hyprland-patched}/share/wayland-sessions";
+        user = "greeter";
+      };
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
 
@@ -161,6 +177,7 @@
     git
     zsh
     sbctl
+    greetd.tuigreet
   ];
 
   services.gvfs.enable = true;
