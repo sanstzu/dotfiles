b9f1d58c66cafd8c049abd457da49827a0f3464d
Clayton Fernalo <claytonfernalo@gmail.com>
2025-02-27 03:05:29 +0800

    feat: remove gnome and gdm

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -81,7 +81,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${pkgs.hyprland-patched}/share/wayland-sessions";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
         user = "greeter";
       };
     };
